2e32d140e68f9b1913906e4ab55f68fdca417287
tejas8668 <134821612+tejas8668@users.noreply.github.com>
2025-02-22 12:04:46 +0530

    Update bypasser.py

--- bypasser.py
+++ bypasser.py
@@ -2511,7 +2511,7 @@ def runurl(url):
     url = url[:-1] if url[-1] == "/" else url
     code = url.split("/")[-1]
     final_url = f"{DOMAIN}/{code}"
-    ref = "https://techybull.in/"
+    ref = "https://yojanadigital.in/"
     h = {"referer": ref}
     resp = client.get(final_url, headers=h)
     soup = BeautifulSoup(resp.content, "html.parser")
