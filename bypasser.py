b4ef327d0d6728a69d651e03b3e3c1a81ed9ef37
tejas8668 <134821612+tejas8668@users.noreply.github.com>
2025-02-22 12:20:34 +0530

    Update bypasser.py

--- bypasser.py
+++ bypasser.py
@@ -2505,7 +2505,7 @@ def kingurl(url, retry=False):
 #####################################################################################################
 # helpers
 
-def runurl(url):
+'''def runurl(url):
     client = cloudscraper.create_scraper(allow_brotli=False)
     DOMAIN = "https://get.runurl.in/"
     url = url[:-1] if url[-1] == "/" else url
@@ -2523,7 +2523,29 @@ def runurl(url):
     try:
         return str(r.json()["url"])
     except BaseException:
+        return "Something went wrong :("'''
+
+def runurl(url):
+    client = cloudscraper.create_scraper(allow_brotli=False)
+    DOMAIN = "https://get.runurl.in/"
+    url = url[:-1] if url[-1] == "/" else url
+    code = url.split("/")[-1]
+    final_url = f"{DOMAIN}/{code}"
+    ref = "https://learna1.bgmi32bitapk.in/"
+    h = {"referer": ref}
+    resp = client.get(final_url, headers=h)
+    soup = BeautifulSoup(resp.content, "html.parser")
+    inputs = soup.find_all("input")
+    data = {input.get("name"): input.get("value") for input in inputs}
+    h = {"x-requested-with": "XMLHttpRequest"}
+    time.sleep(8)
+    r = client.post(f"{DOMAIN}/links/go", data=data, headers=h)
+    try:
+        return str(r.json()["url"])
+    except BaseException:
         return "Something went wrong :("
+
+
 
 # check if present in list
 def ispresent(inlist, url):
