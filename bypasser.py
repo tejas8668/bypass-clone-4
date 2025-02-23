ef99a3fbdb8523d6ff9b3be88bbd404e33fbb67b
tejas8668 <134821612+tejas8668@users.noreply.github.com>
2025-02-23 20:00:43 +0530

    Update bypasser.py

--- bypasser.py
+++ bypasser.py
@@ -2355,7 +2355,7 @@ def rslinks(url):
 
 #########################
 # vipurl
-def vipurl(url):
+'''def vipurl(url):
     client = cloudscraper.create_scraper(allow_brotli=False)
     DOMAIN = "https://count.vipurl.in/"
     url = url[:-1] if url[-1] == "/" else url
@@ -2373,7 +2373,32 @@ def vipurl(url):
     try:
         return r.json()["url"]
     except BaseException:
-        return "Something went wrong :("
+        return "Something went wrong :("'''
+
+def vipurl(url, retry=False):
+    client = cloudscraper.create_scraper(allow_brotli=False)
+    DOMAIN = "https://count.vipurl.in"
+    url = url[:-1] if url[-1] == "/" else url
+    code = url.split("/")[-1]
+    final_url = f"{DOMAIN}/{code}"
+    ref = "https://loanoffer.cc/"
+    h = {"referer": ref}
+    
+    try:
+        resp = client.get(final_url, headers=h)
+        soup = BeautifulSoup(resp.content, "html.parser")
+        inputs = soup.find_all("input")
+        data = {input.get("name"): input.get("value") for input in inputs}
+        h = {"x-requested-with": "XMLHttpRequest"}
+        time.sleep(7)
+        r = client.post(f"{DOMAIN}/links/go", data=data, headers=h)
+        return str(r.json()["url"])
+    except BaseException as e:
+        if not retry:
+            print(f"Error occurred: {e}. Retrying...")
+            return vipurl(url, retry=True)
+        else:
+            return "Something went wrong, Please try again..."
 
 
 
