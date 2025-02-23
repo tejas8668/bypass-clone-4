33579c8be97a31b50a5445c84dd5bd5cf43e080c
tejas8668 <134821612+tejas8668@users.noreply.github.com>
2025-02-23 09:10:58 +0530

    Update bypasser.py

--- bypasser.py
+++ bypasser.py
@@ -2357,7 +2357,7 @@ def rslinks(url):
 # vipurl
 def vipurl(url):
     client = cloudscraper.create_scraper(allow_brotli=False)
-    DOMAIN = "https://count.vipurl.in/"
+    DOMAIN = "https://count.vipurl.in"
     url = url[:-1] if url[-1] == "/" else url
     code = url.split("/")[-1]
     final_url = f"{DOMAIN}/{code}"
@@ -2374,27 +2374,6 @@ def vipurl(url):
         return r.json()["url"]
     except BaseException:
         return "Something went wrong :("
-
-'''def vipurl(url):
-    client = cloudscraper.create_scraper(allow_brotli=False)
-    DOMAIN = "https://count.vipurl.in/"
-    url = url[:-1] if url[-1] == "/" else url
-    code = url.split("/")[-1]
-    final_url = f"{DOMAIN}/{code}"
-    resp = client.get(final_url)
-    soup = BeautifulSoup(resp.content, "html.parser")
-    try:
-        inputs = soup.find(id="go-link").find_all(name="input")
-    except:
-        return "Incorrect Link"
-    data = {input.get("name"): input.get("value") for input in inputs}
-    h = {"x-requested-with": "XMLHttpRequest"}
-    time.sleep(5)
-    r = client.post(f"{DOMAIN}/links/go", data=data, headers=h)
-    try:
-        return r.json()["url"]
-    except:
-        return "Something went wrong :("'''
 
 
 
