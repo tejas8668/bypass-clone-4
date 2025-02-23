9aa42480281c6b98141aad95838a6449432b1af8
tejas8668 <134821612+tejas8668@users.noreply.github.com>
2025-02-23 13:18:30 +0530

    Update bypasser.py

--- bypasser.py
+++ bypasser.py
@@ -2560,6 +2560,27 @@ def bypass_modijiurl(url: str):
             return bypassed_url
         except:
             return "Something went wrong :("
+            
+#primeurl.in bypass
+def primeurl(url):
+    client = cloudscraper.create_scraper(allow_brotli=False)
+    DOMAIN = "https://open.primeurl.in/"
+    url = url[:-1] if url[-1] == "/" else url
+    code = url.split("/")[-1]
+    final_url = f"{DOMAIN}/{code}"
+    ref = "https://loan.bgmiupdate.com.in/"
+    h = {"referer": ref}
+    resp = client.get(final_url, headers=h)
+    soup = BeautifulSoup(resp.content, "html.parser")
+    inputs = soup.find_all("input")
+    data = {input.get("name"): input.get("value") for input in inputs}
+    h = {"x-requested-with": "XMLHttpRequest"}
+    time.sleep(8)
+    r = client.post(f"{DOMAIN}/links/go", data=data, headers=h)
+    try:
+        return str(r.json()["url"])
+    except BaseException:
+        return "Something went wrong :("
 
 
 # check if present in list
@@ -2873,6 +2894,10 @@ def shortners(url):
     elif "https://modijiurl.com/" in url:
         print("entered modijiurl:", url)
         return bypass_modijiurl(url)
+
+    elif "https://primeurl.in/" in url:
+        print("entered primeurl:", url)
+        return primeurl(url)
 
     
     # htpmovies sharespark cinevood
