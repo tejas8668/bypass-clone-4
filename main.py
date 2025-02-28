aff6ab407f654e1772058065852adb166a41b3dc
tejas8668 <134821612+tejas8668@users.noreply.github.com>
2025-02-28 09:48:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -52,9 +52,9 @@ def getenv(var):
     return environ.get(var) or DATA.get(var, None)
 
 
-bot_token = getenv("6716467783:AAFvF7g-b8L8026HFgOL9zTxB3Sq7L_E9Uc")
-api_hash = getenv("354e1dd8e1e3041ee2145196da8d6aac")
-api_id = getenv("27710337")
+bot_token = getenv("TOKEN ")
+api_hash = getenv("HASH ")
+api_id = getenv("ID ")
 app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 with app:
     app.set_bot_commands(
